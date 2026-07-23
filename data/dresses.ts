f05719742ec1d8a8a905e8917a68d0f3a71c330a
unknown <ani.bahdasarian@nure.ua>
2026-07-23 14:59:42 +0100

    Added all wedding dresses

--- data/dresses.ts
+++ data/dresses.ts
@@ -154,4 +154,539 @@ export const dresses: Dress[] = [
     ],
     description: "",
   },
+  {
+  slug: "aurora",
+  name: "Aurora",
+  collection: "Illuminare",
+  collectionSlug: "illuminare",
+  images: [
+    "/images/dresses/illuminare/Aurora/aurora1.jpg",
+    "/images/dresses/illuminare/Aurora/aurora2.jpg",
+    "/images/dresses/illuminare/Aurora/aurora3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "diana",
+  name: "Diana",
+  collection: "Illuminare",
+  collectionSlug: "illuminare",
+  images: [
+    "/images/dresses/illuminare/Diana/diana1.jpg",
+    "/images/dresses/illuminare/Diana/diana2.jpg",
+    "/images/dresses/illuminare/Diana/diana3.jpg",
+    "/images/dresses/illuminare/Diana/diana4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "eliana",
+  name: "Eliana",
+  collection: "Illuminare",
+  collectionSlug: "illuminare",
+  images: [
+    "/images/dresses/illuminare/Eliana/eliana1.jpg",
+    "/images/dresses/illuminare/Eliana/eliana2.jpg",
+    "/images/dresses/illuminare/Eliana/eliana3.jpg",
+    "/images/dresses/illuminare/Eliana/eliana4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "flora",
+  name: "Flora",
+  collection: "Illuminare",
+  collectionSlug: "illuminare",
+  images: [
+    "/images/dresses/illuminare/Flora/flora1.jpg",
+    "/images/dresses/illuminare/Flora/flora2.jpg",
+    "/images/dresses/illuminare/Flora/flora3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "helia",
+  name: "Helia",
+  collection: "Illuminare",
+  collectionSlug: "illuminare",
+  images: [    
+    "/images/dresses/illuminare/Helia/helia1.jpg",
+    "/images/dresses/illuminare/Helia/helia2.jpg",
+    "/images/dresses/illuminare/Helia/helia3.jpg",
+    "/images/dresses/illuminare/Helia/helia4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "selena",
+  name: "Selena",
+  collection: "Illuminare",
+  collectionSlug: "illuminare",
+  images: [
+    "/images/dresses/illuminare/Selena/selena1.jpg",
+    "/images/dresses/illuminare/Selena/selena2.jpg",
+    "/images/dresses/illuminare/Selena/selena3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "abigael",
+  name: "Abigael",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Abigael/Abigael1.jpg",
+    "/images/dresses/radiance/Abigael/Abigael2.jpg",
+    "/images/dresses/radiance/Abigael/Abigael3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "adela",
+  name: "Adela",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Adela/Adela1.jpg",
+    "/images/dresses/radiance/Adela/Adela2.jpg",
+    "/images/dresses/radiance/Adela/Adela3.jpg",
+    "/images/dresses/radiance/Adela/Adela4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "agatha",
+  name: "Agatha",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Agatha/Agatha1.jpg",
+    "/images/dresses/radiance/Agatha/Agatha2.jpg",
+    "/images/dresses/radiance/Agatha/Agatha3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "alegra",
+  name: "Alegra",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Alegra/Alegra1.jpg",
+    "/images/dresses/radiance/Alegra/Alegra2.jpg",
+    "/images/dresses/radiance/Alegra/Alegra3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "alexis",
+  name: "Alexis",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Alexis/Alexis1.jpg",
+    "/images/dresses/radiance/Alexis/Alexis2.jpg",
+    "/images/dresses/radiance/Alexis/Alexis3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "allison",
+  name: "Allison",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Allison/Allison1.jpg",
+    "/images/dresses/radiance/Allison/Allison2.jpg",
+    "/images/dresses/radiance/Allison/Allison3.jpg",
+    "/images/dresses/radiance/Allison/Allison4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "amity",
+  name: "Amity",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Amity/Amity1.jpg",
+    "/images/dresses/radiance/Amity/Amity2.jpg",
+    "/images/dresses/radiance/Amity/Amity3.jpg",
+    "/images/dresses/radiance/Amity/Amity4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "ammie",
+  name: "Ammie",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Ammie/Ammie1.jpg",
+    "/images/dresses/radiance/Ammie/Ammie2.jpg",
+    "/images/dresses/radiance/Ammie/Ammie3.jpg",
+    "/images/dresses/radiance/Ammie/Ammie4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "amora",
+  name: "Amora",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Amora/amora1.jpg",
+    "/images/dresses/radiance/Amora/amora2.jpg",
+    "/images/dresses/radiance/Amora/amora3.jpg",
+    "/images/dresses/radiance/Amora/amora4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "andrea",
+  name: "Andrea",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Andrea/Andrea1.jpg",
+    "/images/dresses/radiance/Andrea/Andrea2.jpg",
+    "/images/dresses/radiance/Andrea/Andrea3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "angelina",
+  name: "Angelina",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Angelina/Angelina1.jpg",
+    "/images/dresses/radiance/Angelina/Angelina2.jpg",
+    "/images/dresses/radiance/Angelina/Angelina3.jpg",
+    "/images/dresses/radiance/Angelina/Angelina4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "anita",
+  name: "Anita",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Anita/Anita1.jpg",
+    "/images/dresses/radiance/Anita/Anita2.jpg",
+    "/images/dresses/radiance/Anita/Anita3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "april",
+  name: "April",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/April/april1.jpg",
+    "/images/dresses/radiance/April/april2.jpg",
+    "/images/dresses/radiance/April/april3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "aria",
+  name: "Aria",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Aria/Aria1.jpg",
+    "/images/dresses/radiance/Aria/Aria2.jpg",
+    "/images/dresses/radiance/Aria/Aria3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "ariella",
+  name: "Ariella",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Ariella/Ariella1.jpg",
+    "/images/dresses/radiance/Ariella/Ariella2.jpg",
+    "/images/dresses/radiance/Ariella/Ariella3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "ashlee",
+  name: "Ashlee",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Ashlee/Ashlee1.jpg",
+    "/images/dresses/radiance/Ashlee/Ashlee2.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "astra",
+  name: "Astra",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Astra/Astra1.jpg",
+    "/images/dresses/radiance/Astra/Astra2.jpg",
+    "/images/dresses/radiance/Astra/Astra3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "athena",
+  name: "Athena",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Athena/Athena1.jpg",
+    "/images/dresses/radiance/Athena/Athena2.jpg",
+    "/images/dresses/radiance/Athena/Athena3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "augusta",
+  name: "Augusta",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Augusta/Augusta1.jpg",
+    "/images/dresses/radiance/Augusta/Augusta2.jpg",
+    "/images/dresses/radiance/Augusta/Augusta3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "aura",
+  name: "Aura",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Aura/Aura1.jpg",
+    "/images/dresses/radiance/Aura/Aura2.jpg",
+    "/images/dresses/radiance/Aura/Aura3.jpg",
+    "/images/dresses/radiance/Aura/Aura4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "avery",
+  name: "Avery",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Avery/Avery1.jpg",
+    "/images/dresses/radiance/Avery/Avery2.jpg",
+    "/images/dresses/radiance/Avery/Avery3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "azure",
+  name: "Azure",
+  collection: "Radiance of Love",
+  collectionSlug: "radiance-of-love",
+  images: [
+    "/images/dresses/radiance/Azure/Azure1.jpg",
+    "/images/dresses/radiance/Azure/Azure2.jpg",
+    "/images/dresses/radiance/Azure/Azure3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "camellia",
+  name: "Camellia",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Camellia/camellia1.jpg",
+    "/images/dresses/spring/Camellia/camellia2.jpg",
+    "/images/dresses/spring/Camellia/camellia3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "cherry-blossom",
+  name: "Cherry Blossom",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Cherry Blossom/cherry blossom1.jpg",
+    "/images/dresses/spring/Cherry Blossom/cherry blossom2.jpg",
+    "/images/dresses/spring/Cherry Blossom/cherry blossom3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "daisy",
+  name: "Daisy",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Daisy/daisy1.jpg",
+    "/images/dresses/spring/Daisy/daisy2.jpg",
+    "/images/dresses/spring/Daisy/daisy3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "forget-me-not",
+  name: "Forget Me Not",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/forget me not/forget-me-not1.JPG",
+    "/images/dresses/spring/forget me not/forget-me-not2.JPG",
+    "/images/dresses/spring/forget me not/forget-me-not3.JPG",
+    "/images/dresses/spring/forget me not/forget-me-not4.JPG",
+  ],
+  description: "",
+},
+
+{
+  slug: "freesia",
+  name: "Freesia",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/fresia/freesia1.jpg",
+    "/images/dresses/spring/fresia/fresia2.jpg",
+    "/images/dresses/spring/fresia/fresia3.jpg",
+    "/images/dresses/spring/fresia/fresia4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "lily",
+  name: "Lily",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Lily/lily1.jpg",
+    "/images/dresses/spring/Lily/lily2.jpg",
+    "/images/dresses/spring/Lily/lily3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "magnolia",
+  name: "Magnolia",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Magnolia/magnolia1.jpg",
+    "/images/dresses/spring/Magnolia/magnolia2.jpg",
+    "/images/dresses/spring/Magnolia/magnolia3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "nemesia",
+  name: "Nemesia",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Nemesia/nemesia1.jpg",
+    "/images/dresses/spring/Nemesia/nemesia2.jpg",
+    "/images/dresses/spring/Nemesia/nemesia3.jpg",
+    "/images/dresses/spring/Nemesia/nemesia4.jpg",
+    "/images/dresses/spring/Nemesia/nemesia5.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "peony",
+  name: "Peony",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Peony/peony1.jpg",
+    "/images/dresses/spring/Peony/peony2.jpg",
+    "/images/dresses/spring/Peony/peony3.jpg",
+    "/images/dresses/spring/Peony/peony4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "primerose",
+  name: "Primerose",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Primerose/primerose1.jpg",
+    "/images/dresses/spring/Primerose/primerose2.jpg",
+    "/images/dresses/spring/Primerose/primerose3.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "violet",
+  name: "Violet",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Violet/violet1.jpg",
+    "/images/dresses/spring/Violet/violet2.jpg",
+    "/images/dresses/spring/Violet/violet3.jpg",
+    "/images/dresses/spring/Violet/violet4.jpg",
+  ],
+  description: "",
+},
+
+{
+  slug: "wisteria",
+  name: "Wisteria",
+  collection: "Spring Flower",
+  collectionSlug: "spring-flower",
+  images: [
+    "/images/dresses/spring/Wisteria/wisteria1.jpg",
+    "/images/dresses/spring/Wisteria/wisteria2.jpg",
+    "/images/dresses/spring/Wisteria/wisteria3.jpg",
+  ],
+  description: "",
+},
+
 ];
